bb2c63581003095cba06a751ada62a5655df954e
Pastew <kpastew@gmail.com>
2016-12-17 17:25:05 +0100

    Changed border to bg color

--- browsergotchi.js
+++ browsergotchi.js
@@ -1,27 +1,27 @@
-document.body.style.border = "5px solid red";
+document.body.style.backgroundColor = "rgba(255,0,0,0.1)";
 
-function showRedBorder(){
-	DURATION = 400
+function showRedBorder(duration, maxAlpha){
+
     var div = $('body');
-    $({alpha:0}).animate({alpha:1}, {
-        duration: DURATION,
+    $({alpha:0}).animate({alpha:maxAlpha}, {
+        duration: duration,
         step: function(){
-            div.css('border-color','rgba(255,0,0,'+this.alpha+')');
+            div.css('backgroundColor','rgba(255,0,0,'+this.alpha+')');
         }
     });
 	
 	setTimeout(function(){
-		$({alpha:1}).animate({alpha:0}, {
-			duration: DURATION*3,
+		$({alpha:maxAlpha}).animate({alpha:0}, {
+			duration: duration*3,
 			step: function(){
-				div.css('border-color','rgba(255,0,0,'+this.alpha+')');
+				div.css('backgroundColor','rgba(255,0,0,'+this.alpha+')');
 			}
 		});
-	}, DURATION*4);
+	}, duration);
 }
 
 function showMonsterWindow(){
 	document.body.innerHTML += '<div style="position:fixed;top:0; right:0;width:150px;height:150px;opacity:0.8;z-index:100;background:#000;">wwwwwww</div>';
 }
-showRedBorder();
+showRedBorder(200, 0.5);
 showMonsterWindow();
