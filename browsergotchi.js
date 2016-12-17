8ea764871fad2a18f9d3b8c3f23b15736eaf2dcf
Pastew <kpastew@gmail.com>
2016-12-17 20:09:12 +0100

    Migrated to localStorage from chrome.storage

--- browsergotchi.js
+++ browsergotchi.js
@@ -18,39 +18,9 @@ function showRedBorder(duration, maxAlpha){
 	}, duration);
 }
 
-function saveData(data){
-	chrome.storage.sync.set({ "data": data }, function(){
-		refreshView();
-	});
-	console.log("Saving data: " + data);
-	console.log(data);
-}
-
-function initStorage(){
-	var data = new Object();
-	data.hp = 150;
-	data.tmp = "blublu";
-	saveData(data);
-}
-
-function decreaseHP(){
-	//chrome.storage.sync.clear();
-	chrome.storage.sync.get("data", function(item){
-		
-		if(!item.data){
-			console.log("This is first run of this app");
-			initStorage();
-			decreaseHP();
-			return;
-		}
-		
-		//else...
-		var hp = item.data.hp;
-		var newHP = hp - 10;
-		item.data.hp = newHP;
-		saveData(item.data);
-	});
-	
+function saveData(){
+	localStorage.setItem('data', JSON.stringify(data));
+	refreshView();
 }
 
 /*
@@ -58,10 +28,38 @@ This function is executed by saveData.
 If you want to update something in view window do it here.
 */
 function refreshView(){
-	chrome.storage.sync.get("data", function(item){
-		var data = item.data;
-		$("#browsergotchi-hp").text("HP: " + data.hp);
-	});	
+	console.log(data.hp);
+	console.log("RefreshView");
+	$("#browsergotchi-hp").text("HP: " + data.hp);	
+}
+
+function initStorage(){	
+	data = JSON.parse(localStorage.getItem('data'));
+	if(!data){
+		console.log("This is first run of this app. Will initialize fields");	
+		data = new Object();
+		data.hp = 150;
+		data.tmp = "blublu";
+		saveData();
+	}
+	else{
+		refreshView();
+	}
+}
+
+function decreaseHP(){
+	var hp = data.hp;
+	var newHP = hp - 10;
+	if(newHP > 0) {
+		data.hp = newHP;
+		saveData(data);
+	} else{
+		onMonsterDeath();
+	}
+}
+
+function onMonsterDeath(){
+	//window.clearInterval(intervalID)
 }
 
 function hit(){
@@ -70,14 +68,20 @@ function hit(){
 }
 
 function showMonsterWindow(){
-	//document.body.innerHTML += '<div id="browsergotchi"></div>';
 	$("body").append('<div id="browsergotchi"></div>');	
 	$("#browsergotchi").draggable();
-	$("#browsergotchi").append('<div id="browsergotchi-hp">HP PLACEHOLDER</div>');
-	refreshView();
-	// TODO: Remember last position. Maybe store x and y in storage.sync?
+	$("#browsergotchi").append('<div id="browsergotchi-hp"></div>');
+	// TODO: Remember last position of the window. Maybe store x and y in storage.sync?
 }
 
+var data;
+localStorage.clear();
+initStorage();
 var TIME_BETWEEN_HIT = 2; // in seconds
 var intervalID = window.setInterval(hit, TIME_BETWEEN_HIT*1000);
+
+$(window).bind('beforeunload', function(){
+  alert('aaa');
+});
+
 showMonsterWindow();
