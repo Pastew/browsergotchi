28455abe5365f12750c5ded0883fce2f9d3a2651
Pastew <kpastew@gmail.com>
2016-12-17 16:13:32 +0100

    Hit animation done stupid way

--- borderify.js
+++ borderify.js
@@ -1,5 +1,20 @@
 document.body.style.border = "5px solid red";
 
-$(document).ready(function(){
-	alert("abc");
-});
+DURATION = 400
+    var div = $('body');
+    $({alpha:0}).animate({alpha:1}, {
+        duration: DURATION,
+        step: function(){
+            div.css('border-color','rgba(255,0,0,'+this.alpha+')');
+        }
+    });
+	
+setTimeout(function(){
+    $({alpha:1}).animate({alpha:0}, {
+        duration: DURATION*3,
+        step: function(){
+            div.css('border-color','rgba(255,0,0,'+this.alpha+')');
+        }
+    });
+}, DURATION*4);
+
