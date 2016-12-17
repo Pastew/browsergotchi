92ea4c1e958c1a3ee8a30e29776d19aa24f062f3
Pastew <kpastew@gmail.com>
2016-12-17 18:35:43 +0100

    storing data and decreasing HP works

--- browsergotchi.js
+++ browsergotchi.js
@@ -18,15 +18,51 @@ function showRedBorder(duration, maxAlpha){
 	}, duration);
 }
 
-function executeShowRedBorder(){
+function saveData(data){
+	chrome.storage.sync.set({ "data": data }, function(){});
+	console.log("Saving data: " + data);
+	console.log(data);
+}
+
+function initStorage(){
+	var data = new Object();
+	data.hp = 150;
+	data.tmp = "blublu";
+	saveData(data);
+}
+
+function decreaseHP(){
+	//chrome.storage.sync.clear();
+	chrome.storage.sync.get("data", function(item){
+		
+		if(!item.data){
+			console.log("This is first run of this app");
+			initStorage();
+			decreaseHP();
+			return;
+		}
+		
+		//else...
+		var hp = item.data.hp;
+		var newHP = hp - 10;
+		item.data.hp = newHP;
+		saveData(item.data);
+	});
+	
+}
+
+function hit(){
 	showRedBorder(200, 0.9);
+	
+	decreaseHP();
 }
 
 function showMonsterWindow(){
 	document.body.innerHTML += '<div id="browsergotchi"></div>';
 	$("#browsergotchi").draggable();
+	// TODO: Remember last position. Maybe store x and y in cookie?
 }
 
 var TIME_BETWEEN_HIT = 2; // in seconds
-var intervalID = window.setInterval(executeShowRedBorder, TIME_BETWEEN_HIT*1000);
+var intervalID = window.setInterval(hit, TIME_BETWEEN_HIT*1000);
 showMonsterWindow();
