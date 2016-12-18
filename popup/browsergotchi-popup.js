e35860c55f955f2bdc9a4f8ef691776a1b3af91a
Pastew <kpastew@gmail.com>
2016-12-18 06:24:27 +0100

    populate list with real data

--- popup/browsergotchi-popup.js
+++ popup/browsergotchi-popup.js
@@ -1,3 +1,22 @@
-/**
- * Created by Pastew on 18.12.2016.
- */
+chrome.storage.sync.get("data", function (item) {
+    if(!item.data){
+        console.log("Can't load storage.sync data in popup! Something is wrong");
+        return;
+    }
+
+    var smartUrls = item.data.smartUrls;
+    var smartUrlsUL = document.getElementById("smartList");
+    for (var i = 0; i < smartUrls.length; i++) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(smartUrls[i]));
+        smartUrlsUL.appendChild(li);
+    }
+
+    var stupidUrls = item.data.stupidUrls;
+    var stupidUrlsUL = document.getElementById("stupidList");
+    for (var i = 0; i < stupidUrls.length; i++) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(stupidUrls[i]));
+        stupidUrlsUL.appendChild(li);
+    }
+});
